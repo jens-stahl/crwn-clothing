3a41fdc684c3a390af69e38daabe0c6ee46cee7b
Jens Stahl <mail@jens-stahl.de>
2021-02-26 14:54:18 +0100

    collection preview

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
+                <Route exact path='/shop' component={ShopPage}/>
             </Switch>
         </div>
     );
 }
-
 export default App;
